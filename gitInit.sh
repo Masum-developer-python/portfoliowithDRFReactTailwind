f028ee1f08305289e2650e4b33b1d62b3f6fab1f
Masum-developer-python <developer.rahmatullah.masum@gmail.com>
2024-12-03 21:59:56 +0600

    Virtual Environment created

---
+++ gitInit.sh
@@ -0,0 +1,7 @@
+echo "# portfoliowithDRFReactTailwind" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Masum-developer-python/portfoliowithDRFReactTailwind.git
+git push -u origin main
